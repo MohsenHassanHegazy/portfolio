fbe1062827590b64b3d05cc15d005c4710e06ade
MohsenHassanHegazy <146730300+MohsenHassanHegazy@users.noreply.github.com>
2024-03-18 15:04:10 +0200

    left navbar

--- js.js
+++ js.js
@@ -8,3 +8,31 @@ handleMouseMove = (event) => {
     ref.style.top = y - scrollTop -450  + 'px';
 };
 document.addEventListener("mousemove", handleMouseMove, false);
+
+activelink =(event)=>{
+    const about =document.getElementById('aboutlink');
+    const courses =document.getElementById('courseslink');
+    const project =document.getElementById('projectslink');
+    const scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+
+    if(scrollTop<about.offsetTop){
+        about.setAttribute('class','active');
+        courses.setAttribute('class','none');
+        project.setAttribute('class','none');
+     }
+     else if(scrollTop>about.offsetTop &&scrollTop<courses.offsetTop+250){
+        about.setAttribute('class','none');
+        courses.setAttribute('class','active');
+        project.setAttribute('class','none');
+     }
+     else if(scrollTop>=courses.offsetTop+250){
+        about.setAttribute('class','noen');
+        courses.setAttribute('class','none');
+        project.setAttribute('class','active');
+      }
+
+ }
+
+ document.addEventListener("scroll",activelink);
+
